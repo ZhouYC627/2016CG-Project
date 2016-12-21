729b10a09cc2e3f99796589060fd2be69b815b09
Zhou.Y.C <answer@answerdeMacBook-Pro.local>
2016-12-21 21:14:53 +0800

    Poly Scan 2nd Version.

---
+++ PolyScan.h
@@ -0,0 +1,13 @@
+//
+//  PolyScan.h
+//  GlutDemo
+//
+//  Created by Zhou.Y.C on 12/21/16.
+//  Copyright © 2016 njucs. All rights reserved.
+//
+
+#ifndef PolyScan_h
+#define PolyScan_h
+
+
+#endif /* PolyScan_h */
